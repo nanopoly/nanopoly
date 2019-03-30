d9a513f081107f04fcbf79cd2713ea3ae16d860e
umu+ <aydinumut82@gmail.com>
2019-03-30 10:47:34 +0300

    service manager added

---
+++ src/service-manager.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const exists = require('fs').existsSync;
+const is = require('is_js');
+const joinPath = require('path').join;
+const readdir = require('fs').readdir;
+
+const Base = require('./base');
+const { ServiceError, NanopolyError } = require('./errors');
+
+class ServiceManager {
+    constructor() {
+        this._services = {};
+    }
+
+    addService(service) {
+        if (is.not.function(service)) throw new ServiceError(service);
+
+        const name = Base.__fixServiceName(service);
+        this._services[name] = service;
+    }
+
+    addServices(services) {
+        if (is.not.array(services)) throw new NanopolyError('you must provide an array of services');
+
+        for (let service of services) this.addService(service);
+    }
+
+    addPath(dir) {
+        if (is.not.string(dir)) throw new NanopolyError('you must provide a path to service classes');
+        else if (!exists(dir)) throw new NanopolyError('invalid path');
+
+        readdir(dir, (error, files) => {
+            if (error) throw error;
+
+            for (let file of files)
+                this.addService(require(joinPath(dir, file)));
+        });
+    }
+
+    isEmpty() {
+        return is.empty(this._services);
+    }
+
+    hasService(service) {
+        if (is.not.string(service) || is.empty(service)) return false;
+
+        return this._services.hasOwnProperty(service);
+    }
+
+    getService(service, method) {
+        if (!this.hasService(service)) return undefined;
+        else if (is.not.string(method) || is.empty(method) || method.charAt(0) === '_')
+            return undefined;
+
+        return this._services[service][method];
+    }
+}
+
+module.exports = ServiceManager;
