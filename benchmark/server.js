84c7a09758c49e322cf01d292690940a6e31bf03
umu+ <aydinumut82@gmail.com>
2019-04-02 09:23:39 +0300

    early stage benchmark implemented

---
+++ benchmark/server.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const Redis = require('ioredis');
+const Server = require('../src/server');
+const ServiceManager = require('../src/service-manager');
+
+class Service {
+    static _name() {
+        return 's';
+    }
+
+    static async echo(request, reply){
+        reply(request);
+    }
+}
+
+const sm = new ServiceManager();
+sm.addService(Service);
+
+const server = new Server(sm, { redis: new Redis(), logs: 'error', iface: 'wlp58s0' });
+server.start();
